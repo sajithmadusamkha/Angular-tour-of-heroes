5dd604139263cd12d2055e3b0101a29d118a0d1a
sajithmadusamkha <sajithmadusankha02@gmail.com>
2023-07-11 12:26:29 +0530

    content updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Sajith Madusankha';
+  imgPath = '../assets/bg.jpg';
+  name = 'Sajith';
+  isdisabled = false;
+  value = 'Submit';
+  inputValue = '';
+
+  passValueToComponent(event: any) {
+    console.log(event.target.value);
+  }
+
+  checkValue() {
+    if (this.inputValue === this.name) {
+      this.isdisabled = true;
+    } else {
+      this.isdisabled = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    SideNavComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
